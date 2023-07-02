9978e8ec50495b29c177effd3285fabd10dc1f27
jalvsaker <jalvsaker@users.noreply.github.com>
2023-07-02 02:54:48 +0200

    isr only on serverless

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,7 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			runtime: "edge",
-			regions: ["arn1"]
-		})
+		adapter: adapter()
 	}
 };
 
