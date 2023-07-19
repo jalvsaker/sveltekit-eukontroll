a7874884ef630d24121416cd3852dc74baf5ccd0
jalvsaker <jalvsaker@users.noreply.github.com>
2023-07-19 17:54:21 +0200

    cache change

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -10,5 +10,5 @@ export const actions = {
 };
 
 export function load({setHeaders}){
-	setHeaders({ 'Cache-Control': 's-maxage=31536000, immutable' });
+	setHeaders({ 'Cache-Control': 's-maxage=31536000' });
 }
